26c22efc29f0b002e3ac9fcf3116109daf764261
Aleksandr <Vespin1991@bk.ru>
2022-09-23 01:24:59 +0300

    dz47/1

--- dz47/Program.cs
+++ dz47/Program.cs
@@ -4,13 +4,13 @@ Console.WriteLine("Введите колличество строк m : ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите колличество столбцов : ");
 int n = Convert.ToInt32(Console.ReadLine());
-double[,] massiv = new double[m,n];
- for (int i = 0; i < m; i++)
- {
-    for (int j = 0; j < n; j++)
+double[,] massiv = new double[m, n];
+for (int i = 0; i < massiv.GetLength(0); i++)
+{
+    for (int j = 0; j < massiv.GetLength(1); j++)
     {
-        massiv[i, j]=Math.Round(new Random().NextDouble() * (-11 + (-10)) - (-10), 1);;
-                Console.Write($"{massiv[i, j]}  ");
+        massiv[i, j] = Math.Round(new Random().NextDouble() * (-11 + (-10)) - (-10), 1); ;
+        Console.Write($"{massiv[i, j]}  ");
     }
     Console.WriteLine();
- }
+}
